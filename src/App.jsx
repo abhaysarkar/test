c5ecdb68a6ef8370d7c426dfbb7bd95957abfeee
abhay <abhaysheets@gmail.com>
2024-08-26 15:42:07 +0530

    jai shree radhe krishna

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from '../src/components/NavBar.jsx'
-import Main from '../src/components/Main.jsx'
-import Page1 from '../src/components/Page1.jsx'
-import Page2 from '../src/components/Page2.jsx'
-import Page3 from '../src/components/Page3.jsx'
-import Page4 from '../src/components/Page4.jsx'
-import Page5 from '../src/components/Page5.jsx'
+// import { useState } from 'react'
+// import './App.css'
+// import NavBar from './components/NavBar.jsx'
+// import Main from './components/Main.jsx'
+// import Page1 from './components/Page1.jsx'
+// import Page2 from './components/Page2.jsx'
+// import Page3 from './components/Page3.jsx'
+// import Page4 from './components/Page4.jsx'
+// import Page5 from './components/Page5.jsx'
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// function App() {
+ 
+
+//   return (
+//     <>
+//       <NavBar />
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Main />} />
+//           <Route path="/page1" element={<Page1 />} />
+//           <Route path="/page2" element={<Page2 />} />
+//           <Route path="/page3" element={<Page3 />} />
+//           <Route path="/page4" element={<Page4 />} />
+//           <Route path="/page5" element={<Page5 />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+import { useState } from 'react';
+import './App.css';
+import NavBar from './components/NavBar.jsx';
+import Main from './components/Main.jsx';
+import Page1 from './components/Page1.jsx';
+import Page2 from './components/Page2.jsx';
+import Page3 from './components/Page3.jsx';
+import Page4 from './components/Page4.jsx';
+import Page5 from './components/Page5.jsx';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
- 
-
   return (
-    <>
+    <Router>
       <NavBar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/page3" element={<Page3 />} />
-          <Route path="/page4" element={<Page4 />} />
-          <Route path="/page5" element={<Page5 />} />
-        </Routes>
-      </Router>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+        <Route path="/page5" element={<Page5 />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,143 @@
+// import { useNavigate } from 'react-router-dom'
+// const NavBar = () => {
+//     const navigate = useNavigate();
+//     return (
+//         <>
+//             <nav className="navbar navbar-dark bg-primary navbar-expand-lg fixed-top">
+//                 <div className="container-fluid">
+//                     <a className="navbar-brand" href="#">Navbar</a>
+//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span className="navbar-toggler-icon"></span>
+//                     </button>
+//                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//                         <ul className="navbar-nav">
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" onClick={() => navigate("/page1")}>Page1</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" onClick={() => navigate("/page2")}>Page2</a>
+//                             </li>
+
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" onClick={() => navigate("/page3")}>Page3</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" onClick={() => navigate("/page4")}>Page4</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link active" onClick={() => navigate("/page5")}>Page5</a>
+//                             </li>
+
+//                         </ul>
+//                         <div>
+//                             <button className="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Add New</button>
+//                         </div>
+
+
+
+//                     </div>
+//                 </div>
+//             </nav>
+//             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//                 <div className="modal-dialog">
+//                     <div className="modal-content">
+//                         <div className="modal-header">
+//                             <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+
+//                         </div>
+//                         <div className="modal-body">
+//                             <h3>Custom Domain</h3>
+//                             <p>for custom domain we need some cridentials for that need to be setup</p>
+//                         </div>
+//                         <div className="modal-footer">
+//                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//                             <button type="button" className="btn btn-primary">Save changes</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+
+//     );
+// }
+
+// export default NavBar;
+
+
+
+
+import { useNavigate } from 'react-router-dom';
+
 const NavBar = () => {
+    const navigate = useNavigate();
     return (
         <>
             <nav className="navbar navbar-dark bg-primary navbar-expand-lg fixed-top">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link" href="/" aria-current="page">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="page1">Page1</a>
+                                <a className="nav-link" onClick={() => navigate("/page1")}>Page1</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="page2">Page2</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link active" href="page3">Page3</a>
+                                <a className="nav-link" onClick={() => navigate("/page2")}>Page2</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="page4">Page4</a>
+                                <a className="nav-link" onClick={() => navigate("/page3")}>Page3</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="page5">Page5</a>
+                                <a className="nav-link" onClick={() => navigate("/page4")}>Page4</a>
                             </li>
-
+                            <li className="nav-item">
+                                <a className="nav-link" onClick={() => navigate("/page5")}>Page5</a>
+                            </li>
                         </ul>
-                        <div>
-                            <button className="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Add New</button>
+                        <div className="ms-auto">
+                            <button
+                                className="btn btn-outline-warning"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal"
+                            >
+                                Add New
+                            </button>
                         </div>
-
-
-
                     </div>
                 </div>
             </nav>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+            <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-
                         </div>
                         <div className="modal-body">
                             <h3>Custom Domain</h3>
-                            <p>for custom domain we need some cridentials for that need to be setup</p>
+                            <p>For a custom domain, we need some credentials to be set up.</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -58,8 +147,7 @@ const NavBar = () => {
                 </div>
             </div>
         </>
-
     );
-}
+};
 
 export default NavBar;
